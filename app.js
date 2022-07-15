d8a9b74ebeaad277e2f28c8b0e77e9a7074584c3
Muhammad ibrahim <Muhammadibrahimtanoli@outlook.com>
2022-07-15 19:01:10 +0500

    changes added

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://eagle1103.startdedicated.com',
+  })
+)
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
