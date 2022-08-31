c8a2a28cdb63fb57fda7871387401da0930d7334
imran.khan@imogent.com <imran.khan@imogent.com>
2022-08-31 22:11:03 +0500

    added user log

--- routes/users.js
+++ routes/users.js
@@ -2,49 +2,72 @@ var express = require('express')
 var router = express.Router()
 var User = require('../models/user')
 var jwt = require('jsonwebtoken')
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://127.0.0.1:27017/'
 
 router.post('/register', function (req, res, next) {
-  var user = new User({
+  var user = {
     email: req.body.email,
     username: req.body.username,
     password: User.hashPassword(req.body.password),
     creation_dt: Date.now(),
-  })
+  };
 
-  let promise = user.save()
-
-  promise.then(function (doc) {
-    return res.status(201).json(doc)
-  })
-
-  promise.catch(function (err) {
-    return res.status(501).json({ message: 'Error registering user.' })
-  })
+  MongoClient.connect(url, function (err, db) {
+    var dbo = db.db('mydb')
+    dbo
+    .collection('User')
+    .insertOne(user, function (err, result) {
+      db.close()
+      if (err){
+        return res.status(501).json({ message: 'Error registering user.' })
+      }
+      else{
+        return res.status(201).json(user);
+      }
+    })
+  });
 })
 
 router.post('/login', function (req, res, next) {
-  let promise = User.findOne({ email: req.body.email }).exec()
-
-  promise.then(function (doc) {
-    if (doc) {
-      if (doc.isValid(req.body.password)) {
-        // generate token
-        let token = jwt.sign({ username: doc.username }, 'secret', {
-          expiresIn: '3h',
-        })
-
-        return res.status(200).json(token)
-      } else {
-        return res.status(401).json({ message: ' Invalid Credentials' })
+  console.log('in login',req.body);
+  MongoClient.connect(url, function (err, db) {
+    var dbo = db.db('mydb')
+    dbo
+    .collection('User')
+    .findOne({ email: req.body.email }, function (err, result) {
+      db.close()
+      if (err){
+        return res.status(501).json({ message: 'Error registering user.' })
       }
-    } else {
-      return res.status(501).json({ message: 'User email is not registered.' })
-    }
-  })
+      else{
+        return res.status(201).json(result);
+      }
+    })
+  });
 
-  promise.catch(function (err) {
-    return res.status(501).json({ message: 'Some internal error' })
-  })
+  // let promise = User.findOne({ email: req.body.email }).exec()
+
+  // promise.then(function (doc) {
+  //   if (doc) {
+  //     if (doc.isValid(req.body.password)) {
+  //       // generate token
+  //       let token = jwt.sign({ username: doc.username }, 'secret', {
+  //         expiresIn: '3h',
+  //       })
+
+  //       return res.status(200).json(token)
+  //     } else {
+  //       return res.status(401).json({ message: ' Invalid Credentials' })
+  //     }
+  //   } else {
+  //     return res.status(501).json({ message: 'User email is not registered.' })
+  //   }
+  // })
+
+  // promise.catch(function (err) {
+  //   return res.status(501).json({ message: 'Some internal error' })
+  // })
 })
 
 router.get('/username', verifyToken, function (req, res, next) {
